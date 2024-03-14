3ad43164d6d7779e59ac9a7d0b8069a2d3aadff8
M. Kozlik <malkak@skyvar.co.il>
2024-03-14 15:39:58 +0200

    Change the storage acount resource to data

--- user-disable/main.tf
+++ user-disable/main.tf
@@ -3,13 +3,19 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.rg_location
 }
 
-resource "azurerm_storage_account" "storage_account" {
+# resource "azurerm_storage_account" "storage_account" {
+#   name                     = var.storage_account_name
+#   resource_group_name      = azurerm_resource_group.resource_group.name
+#   location                 = azurerm_resource_group.resource_group.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+# ------------------------------------------------------------------------
+data "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 }
+# ---------------------------------------------------------------------------
 
 data "azurerm_key_vault" "key_vault" {
   name                = var.key_vault_name
