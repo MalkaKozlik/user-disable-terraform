6dd293d7ddf5869fb12fa3bdbf620157f0962c8f
M. Kozlik <malkak@skyvar.co.il>
2024-03-14 13:05:43 +0200

    TRy

--- administrator/main.tf
+++ administrator/main.tf
@@ -9,33 +9,32 @@ resource "azurerm_storage_account" "storage_account" {
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  depends_on = [ azurerm_resource_group.resource_group ]
 }
 
-# data "azurerm_client_config" "current_client" {}
+data "azurerm_client_config" "current_client" {}
 
-# resource "azurerm_key_vault" "key_vault" {
-#   name                       = var.key_vault_name
-#   location                   = var.rg_location
-#   resource_group_name        = azurerm_storage_account.storage_account.resource_group_name
-#   soft_delete_retention_days = 7
-#   tenant_id                  = data.azurerm_client_config.current_client.tenant_id
-#   sku_name                   = var.key_vault_sku_name
+resource "azurerm_key_vault" "key_vault" {
+  name                       = var.key_vault_name
+  location                   = var.rg_location
+  resource_group_name        = azurerm_storage_account.storage_account.resource_group_name
+  soft_delete_retention_days = 7
+  tenant_id                  = data.azurerm_client_config.current_client.tenant_id
+  sku_name                   = var.key_vault_sku_name
 
-#   access_policy {
-#     tenant_id = data.azurerm_client_config.current_client.tenant_id
-#     object_id = data.azurerm_client_config.current_client.object_id
-#     certificate_permissions = var.key_vault_certificate_permissions
-#     key_permissions = var.key_vault_key_permissions
-#     secret_permissions = var.key_vault_secret_permissions
-#     storage_permissions = var.key_vault_storage_permissions
-#   }
-# }
+  access_policy {
+    tenant_id = data.azurerm_client_config.current_client.tenant_id
+    object_id = data.azurerm_client_config.current_client.object_id
+    certificate_permissions = var.key_vault_certificate_permissions
+    key_permissions = var.key_vault_key_permissions
+    secret_permissions = var.key_vault_secret_permissions
+    storage_permissions = var.key_vault_storage_permissions
+  }
+}
 
-# resource "azurerm_key_vault_secret" "key_vault_secret" {
-#   name         = var.key_vault_secret_name
-#   value        = azurerm_storage_account.storage_account.primary_connection_string
-#   key_vault_id = azurerm_key_vault.key_vault.id
-# }
+resource "azurerm_key_vault_secret" "key_vault_secret" {
+  name         = var.key_vault_secret_name
+  value        = azurerm_storage_account.storage_account.primary_connection_string
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
 
 
--- main-project/main.tf
+++ main-project/main.tf
@@ -23,17 +23,17 @@ module administrator {
 
 
 
-# module user-disable {
-#   source = "../user-disable/"
-#   key_vault_name = module.administrator.key_vault_name
-#   key_vault_resource_group_name = module.administrator.key_vault_resource_group_name
-#   DOCKER_REGISTRY_SERVER_URL = var.DOCKER_REGISTRY_SERVER_URL
-#   DOCKER_REGISTRY_SERVER_USERNAME = var.DOCKER_REGISTRY_SERVER_USERNAME
-#   DOCKER_REGISTRY_SERVER_PASSWORD = var.DOCKER_REGISTRY_SERVER_PASSWORD
+module user-disable {
+  source = "../user-disable/"
+  key_vault_name = module.administrator.key_vault_name
+  key_vault_resource_group_name = module.administrator.key_vault_resource_group_name
+  DOCKER_REGISTRY_SERVER_URL = var.DOCKER_REGISTRY_SERVER_URL
+  DOCKER_REGISTRY_SERVER_USERNAME = var.DOCKER_REGISTRY_SERVER_USERNAME
+  DOCKER_REGISTRY_SERVER_PASSWORD = var.DOCKER_REGISTRY_SERVER_PASSWORD
 
-#   depends_on = [
-#       module.administrator
-#   ]
-# }
+  depends_on = [
+      module.administrator
+  ]
+}
 
 
